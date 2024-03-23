70d676fde72cf2ec04e1ffcdac7c37a0d6fbbd73
kurko67 <maxi.alaniz@gmail.com>
2024-03-23 17:10:24 -0300

    update notifications.html

--- src/main/java/com/kurko67/controlflotas/controllers/NotificationsController.java
+++ src/main/java/com/kurko67/controlflotas/controllers/NotificationsController.java
@@ -1,10 +1,14 @@
 package com.kurko67.controlflotas.controllers;
 
 import com.kurko67.controlflotas.models.dao.INotificacionDao;
+import com.kurko67.controlflotas.models.dao.IUsuarioDao;
 import com.kurko67.controlflotas.models.entity.Notificacion;
+import com.kurko67.controlflotas.models.entity.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class NotificationsController {
@@ -20,12 +26,24 @@ public class NotificationsController {
     @Autowired
     private INotificacionDao notificacionDao;
 
-    @GetMapping("/notificaciones")
-    public List<String> mostrarNotificaciones() {
-        List<String> asuntos = notificacionDao.findAllAsuntos();
-        return asuntos;
-    }
+    @Autowired
+    IUsuarioDao usuariodao;
 
+    @GetMapping("/notificaciones")
+    public Map<Long, String> mostrarNotificaciones(@AuthenticationPrincipal User user) {
+
+        Usuario receptor = usuariodao.findByUsername(user.getUsername());
+        Pageable pageable = PageRequest.of(0, 5); // Obtener las primeras 5 notificaciones
+
+        List<Object[]> asuntosYMensajes = notificacionDao.findLastFiveByReceptor(receptor, pageable);
+        Map<Long, String> notificaciones = new HashMap<>();
+        for (Object[] asuntoYMensaje : asuntosYMensajes) {
+            Long id = (Long) asuntoYMensaje[0]; // Suponiendo que el ID está en la primera posición del arreglo
+            String asunto = (String) asuntoYMensaje[1];
+            notificaciones.put(id, asunto);
+        }
+        return notificaciones;
+    }
 
 
 }
--- src/main/java/com/kurko67/controlflotas/models/dao/INotificacionDao.java
+++ src/main/java/com/kurko67/controlflotas/models/dao/INotificacionDao.java
@@ -1,15 +1,18 @@
 package com.kurko67.controlflotas.models.dao;
 
 import com.kurko67.controlflotas.models.entity.Notificacion;
+import com.kurko67.controlflotas.models.entity.Usuario;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface INotificacionDao extends CrudRepository<Notificacion, Long> {
 
-    @Query("SELECT n.asunto, n.mensaje FROM Notificacion n")
-    List<String> findAllAsuntos();
+    @Query("SELECT n.idNotificacion, n.asunto FROM Notificacion n WHERE n.receptor = :receptor ORDER BY n.created_at DESC")
+    List<Object[]> findLastFiveByReceptor(@Param("receptor") Usuario receptor, Pageable pageable);
 
 
 }
