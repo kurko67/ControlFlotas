3bdb5927ac666a22e2dfedd3b817742904f0eddd
kurko67 <maxi.alaniz@gmail.com>
2024-03-28 20:54:36 -0300

    Finish auto-task notifications

--- src/main/java/com/kurko67/controlflotas/controllers/DashBoardController.java
+++ src/main/java/com/kurko67/controlflotas/controllers/DashBoardController.java
@@ -1,17 +1,33 @@
 package com.kurko67.controlflotas.controllers;
 
+import com.kurko67.controlflotas.models.dao.INotificacionDao;
+import com.kurko67.controlflotas.models.dao.IUsuarioDao;
+import com.kurko67.controlflotas.models.entity.Conductor;
+import com.kurko67.controlflotas.models.service.IConductorService;
+import com.kurko67.controlflotas.models.service.IVehiculoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
+
 @Controller()
 public class DashBoardController {
 
+    @Autowired
+    private IConductorService conductorService;
+    @Autowired
+    private IVehiculoService vehiculoService;
 
     @GetMapping("/")
     public String panelAdministrador(Model model, RedirectAttributes flash){
+
+        List<Conductor> conductores = conductorService.findConductoresPorVencerLicencia();
+
+        model.addAttribute("conductores", conductores);
 
         return "index";
 
--- src/main/java/com/kurko67/controlflotas/models/dao/IVehiculoDao.java
+++ src/main/java/com/kurko67/controlflotas/models/dao/IVehiculoDao.java
@@ -25,5 +25,10 @@ public interface IVehiculoDao extends PagingAndSortingRepository<Vehiculo, Long>
     @Query(value = "select * from vehiculos where vencimiento_seguro between current_date and current_date + interval 30 day", nativeQuery = true)
     List<Vehiculo> findVehiculoBySeguroExpire();
 
+    @Query(value = "SELECT id, vehiculo, 'VTV' AS tipo_vencimiento, vencimiento_vtv AS fecha_vencimiento " +
+            "FROM tu_tabla " +
+            "WHERE vencimiento_vtv BETWEEN CURRENT_DATE AND CURRENT_DATE + INTERVAL 30 DAY", nativeQuery = true)
+    List<Vehiculo> findExpires();
+
 
 }
