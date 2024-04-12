47d29ab352ca52bfca52b9fcbb827bba45926f1f
kurko67 <maxi.alaniz@gmail.com>
2024-04-12 14:57:14 -0300

    update

--- src/main/java/com/kurko67/controlflotas/ControlflotasApplication.java
+++ src/main/java/com/kurko67/controlflotas/ControlflotasApplication.java
@@ -1,7 +1,9 @@
 package com.kurko67.controlflotas;
 
+import com.kurko67.controlflotas.util.paginator.WhatsAppService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
